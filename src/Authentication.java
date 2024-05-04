6e7f87d46ad3daf4b05ebc8b83f02030b0c2f2a6
Ali Hodroj <aloshito2004@gmail.com>
2024-05-05 01:23:12 +0300

    Added Authentication Functionality

--- src/Authentication.java
+++ src/Authentication.java
@@ -1,17 +1,328 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public abstract class Authentication {
-    public static void studentLogin() {
-
+    static final private Scanner sc = new Scanner(System.in);
+    public static void studentLogin(Data dataModel) {
+        // loop variables
+        boolean loggedIn = false;
+        String usernameInput = "";
+        String passwordInput = "";
+        // main loop
+        do {
+            // display message
+            // taking username
+            System.out.println("You are logging in as a STUDENT");
+            System.out.print("USERNAME: ");
+            usernameInput = sc.nextLine();
+            // error handling
+            while (usernameInput.isEmpty()) {
+                displayDottedLines();
+                System.out.print("Enter a valid username: ");
+                usernameInput = sc.nextLine();
+            }
+            // display message
+            // taking password
+            displayDottedLines();
+            System.out.print("PASSWORD: ");
+            passwordInput = sc.nextLine();
+            while (passwordInput.isEmpty()) {
+                displayDottedLines();
+                System.out.print("Enter a valid password: ");
+                passwordInput = sc.nextLine();
+            }
+            // checking login info
+            displayDottedLines();
+            System.out.println("Checking login info...");
+            Student currentUser = checkIfStudentExists(usernameInput, passwordInput, dataModel);
+            // checking returned student
+            if(!currentUser.getUsername().isEmpty()) {
+                System.out.println("Successfully logged in!");
+                displayDottedLines();
+                // stopping loop
+                loggedIn = true;
+                // loading student menu
+                currentUser.loadMainMenu();
+            } else {
+                // looping back to start
+                System.out.println("Login failed, try again!");
+                displayDottedLines();
+            }
+        } while(!loggedIn);
     }
 
-    public static void instructorLogin() {
-
+    public static void instructorLogin(Data dataModel) {
+        // loop variables
+        boolean loggedIn = false;
+        String usernameInput = "";
+        String passwordInput = "";
+        // username loop
+        do {
+            // display message
+            // taking username
+            System.out.println("You are logging in as an INSTRUCTOR");
+            System.out.print("USERNAME: ");
+            usernameInput = sc.nextLine();
+            // error handling
+            while (usernameInput.isEmpty()) {
+                displayDottedLines();
+                System.out.print("Enter a valid username: ");
+                usernameInput = sc.nextLine();
+            }
+            // display message
+            // taking password
+            displayDottedLines();
+            System.out.print("PASSWORD: ");
+            passwordInput = sc.nextLine();
+            while (passwordInput.isEmpty()) {
+                displayDottedLines();
+                System.out.print("Enter a valid password: ");
+                passwordInput = sc.nextLine();
+            }
+            // checking login info
+            displayDottedLines();
+            System.out.println("Checking login info...");
+            Instructor currentUser = checkIfInstructorExists(usernameInput, passwordInput, dataModel);
+            // checking returned instructor
+            if(!currentUser.getUsername().isEmpty()) {
+                System.out.println("Successfully logged in!");
+                displayDottedLines();
+                // stopping loop
+                loggedIn = true;
+                // loading instructor menu
+                currentUser.loadMainMenu();
+            } else {
+                // looping back to start
+                System.out.println("Login failed, try again!");
+                displayDottedLines();
+            }
+        } while(!loggedIn);
     }
 
-    public static void createStudent() {
-
+    public static void createStudent(Data dataModel) {
+        // loop variables
+        boolean userCreated = false;
+        String fname = "";
+        String lname = "";
+        String passwordInput = "";
+        String confirmPasswordInput = "";
+        int attempts = 3;
+        // main loop
+        do {
+            // display message
+            // taking first name
+            System.out.println("Creating a STUDENT ACCOUNT");
+            System.out.print("Enter your first name: ");
+            fname = sc.nextLine();
+            // error handling
+            while (fname.isEmpty()) {
+                displayDottedLines();
+                System.out.println("Field shouldn't be empty!");
+                System.out.print("Enter a valid first name: ");
+                fname = sc.nextLine();
+            }
+            // taking last name
+            displayDottedLines();
+            System.out.print("Enter your last name: ");
+            lname = sc.nextLine();
+            // error handling
+            while (lname.isEmpty()) {
+                displayDottedLines();
+                System.out.println("Field shouldn't be empty!");
+                System.out.print("Enter a valid last name: ");
+                lname = sc.nextLine();
+            }
+            // taking password
+            displayDottedLines();
+            System.out.print("Enter your password (8 characters or more) : ");
+            passwordInput = sc.nextLine();
+            while (passwordInput.length() <= 8) {
+                displayDottedLines();
+                System.out.print("Enter a valid password (8 characters or more) : ");
+                passwordInput = sc.nextLine();
+            }
+            // taking confirm password
+            displayDottedLines();
+            System.out.print("Confirm password: ");
+            confirmPasswordInput = sc.nextLine();
+            while (!confirmPasswordInput.equals(passwordInput)) {
+                attempts--;
+                if(attempts == 0) { break; }
+                displayDottedLines();
+                System.out.print("Passwords don't match, try again (" + attempts + " attempts left ) : ");
+                confirmPasswordInput = sc.nextLine();
+            }
+            // checking sign up info
+            Random rand = new Random();
+            // creating username
+            String username = fname + "_" + lname + "_" + rand.nextInt(1000, 10000);
+            System.out.println("Creating account...");
+            if(confirmPasswordInput.equals(passwordInput) && checkIfStudentUsernameUnique(username, dataModel)) {
+                // creating new student
+                Student s = new Student(username, confirmPasswordInput);
+                // adding new student into data model
+                dataModel.getStudents().add(s);
+                // saving new student
+                dataModel.saveData();
+                // stopping loop
+                System.out.println("STUDENT created!");
+                System.out.println("Your new USERNAME is (" + s.getUsername() + ") Please memorize it!");
+                displayDottedLines();
+                userCreated = true;
+                // starting main menu
+                s.loadMainMenu();
+            } else {
+                System.out.println("Failed to create STUDENT, try again.");
+                if (!checkIfStudentUsernameUnique(username, dataModel)) {
+                    System.out.println("USERNAME ALREADY EXISTS!");
+                }
+                displayDottedLines();
+            }
+        } while (!userCreated);
     }
 
-    public static void createInstructor() {
+    public static void createInstructor(Data dataModel) {
+        // loop variables
+        boolean userCreated = false;
+        String fname = "";
+        String lname = "";
+        String passwordInput = "";
+        String confirmPasswordInput = "";
+        int attempts = 3;
+        // main loop
+        do {
+            // display message
+            // taking first name
+            System.out.println("Creating an INSTRUCTOR ACCOUNT");
+            System.out.print("Enter your first name: ");
+            fname = sc.nextLine();
+            // error handling
+            while (fname.isEmpty()) {
+                displayDottedLines();
+                System.out.println("Field shouldn't be empty!");
+                System.out.print("Enter a valid first name: ");
+                fname = sc.nextLine();
+            }
+            // taking last name
+            displayDottedLines();
+            System.out.print("Enter your last name: ");
+            lname = sc.nextLine();
+            // error handling
+            while (lname.isEmpty()) {
+                displayDottedLines();
+                System.out.println("Field shouldn't be empty!");
+                System.out.print("Enter a valid last name: ");
+                lname = sc.nextLine();
+            }
+            // taking password
+            displayDottedLines();
+            System.out.print("Enter your password (8 characters or more) : ");
+            passwordInput = sc.nextLine();
+            while (passwordInput.length() <= 8) {
+                displayDottedLines();
+                System.out.print("Enter a valid password (8 characters or more) : ");
+                passwordInput = sc.nextLine();
+            }
+            // taking confirm password
+            displayDottedLines();
+            System.out.print("Confirm password: ");
+            confirmPasswordInput = sc.nextLine();
+            while (!confirmPasswordInput.equals(passwordInput)) {
+                attempts--;
+                if(attempts == 0) { break; }
+                displayDottedLines();
+                System.out.print("Passwords don't match, try again (" + attempts + " attempts left ) : ");
+                confirmPasswordInput = sc.nextLine();
+            }
+            // checking sign up info
+            Random rand = new Random();
+            // creating username
+            String username = fname + "_" + lname + "_" + rand.nextInt(1000, 10000);
+            System.out.println("Creating account...");
+            if(confirmPasswordInput.equals(passwordInput) && checkIfInstructorUsernameUnique(username, dataModel)) {
+                // creating new instructor
+                Instructor i = new Instructor(username, confirmPasswordInput);
+                // adding new student into data model
+                dataModel.getInstructors().add(i);
+                // saving new instructor
+                dataModel.saveData();
+                // stopping loop
+                System.out.println("INSTRUCTOR created!");
+                System.out.println("Your new USERNAME is (" + i.getUsername() + ") Please memorize it!");
+                displayDottedLines();
+                userCreated = true;
+                // starting main menu
+                i.loadMainMenu();
+            } else {
+                System.out.println("Failed to create INSTRUCTOR, try again.");
+                if (!checkIfStudentUsernameUnique(username, dataModel)) {
+                    System.out.println("USERNAME ALREADY EXISTS!");
+                }
+                displayDottedLines();
+            }
+        } while (!userCreated);
+    }
 
+    // HELPER METHODS
+    private static void displayDottedLines() {
+        System.out.println("********************************************");
+    }
+    private static Student checkIfStudentExists(String username, String password, Data dataModel) {
+        // user if found to be saved here
+        Student student = new Student("", "");
+        // checking
+        for (Student s : dataModel.getStudents()) {
+            // check username matching
+            if(s.getUsername().equals(username)) {
+                // check if password matching
+                if(s.getPassword().equals(password)) {
+                    // user found
+                    student = s;
+                }
+            }
+        }
+        return student;
+    }
+    private static Instructor checkIfInstructorExists(String username, String password, Data dataModel) {
+        // user if found to be saved here
+        Instructor instructor = new Instructor("", "");
+        // checking
+        for (Instructor i : dataModel.getInstructors()) {
+            // check username matching
+            if(i.getUsername().equals(username)) {
+                // check if password matching
+                if(i.getPassword().equals(password)) {
+                    // user found
+                    instructor = i;
+                }
+            }
+        }
+        return instructor;
+    }
+    private static boolean checkIfStudentUsernameUnique(String username, Data dataModel) {
+        // initially set to true
+        boolean isUsernameUnique = true;
+        // looping over current students
+        for (Student s : dataModel.getStudents()) {
+            // checking username if equal
+            if (s.getUsername().equals(username)) {
+                isUsernameUnique = false;
+                break;
+            }
+        }
+        return isUsernameUnique;
+    }
+    private static boolean checkIfInstructorUsernameUnique(String username, Data dataModel) {
+        // initially set to true
+        boolean isUsernameUnique = true;
+        // looping over current instructors
+        for (Instructor i : dataModel.getInstructors()) {
+            // checking username if equal
+            if (i.getUsername().equals(username)) {
+                isUsernameUnique = false;
+                break;
+            }
+        }
+        return isUsernameUnique;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,22 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        // Creating main data model and loading DATA
+        Data mainDataModel = new Data();
+        mainDataModel.loadData();
 
-        // creating initial data model
-        Data data = new Data();
-        // loading data
-        data.loadData();
-        /*
         // Authentication loop
         Scanner sc = new Scanner(System.in);
         int input = 0;
-
+        // Main menu loop
         do {
             // display message
-            System.out.println("Welcome! \n 1) Login \n 2) Sign Up \n 3) Exit");
+            System.out.println("Welcome to QUIZY! \n 1) Login \n 2) Sign Up \n 3) Exit");
             System.out.print("Choice: ");
             input = sc.nextInt();
             // handling invalid input
             while(input <= 0 || input > 3) {
+                displayDottedLine();
                 System.out.print("Invalid choice, enter again: ");
                 input = sc.nextInt();
             }
@@ -28,26 +27,31 @@ public class Main {
                 case 1:
                     int input2 = 0;
                     // display message
+                    displayDottedLine();
                     System.out.println("Choose your account type: \n 1) Student \n 2) Instructor \n 3) Back");
                     System.out.print("Choice: ");
                     // taking input2
                     input2 = sc.nextInt();
                     // handling invalid input
                     while(input2 <= 0 || input2 > 3) {
+                        displayDottedLine();
                         System.out.print("Invalid choice, enter again: ");
                         input2 = sc.nextInt();
                     }
                     // switching on input2
                     switch (input2) {
                         case 1:
-                            Authentication.studentLogin();
+                            displayDottedLine();
+                            Authentication.studentLogin(mainDataModel);
                             input = 3;
                             break;
                         case 2:
-                            Authentication.instructorLogin();
+                            displayDottedLine();
+                            Authentication.instructorLogin(mainDataModel);
                             input = 3;
                             break;
                         case 3:
+                            displayDottedLine();
                             break;
                     }
                     break;
@@ -55,45 +59,44 @@ public class Main {
                 case 2:
                     int input3 = 0;
                     // display message
+                    displayDottedLine();
                     System.out.println("Choose the type of account your want to create: \n 1) Student \n 2) Instructor \n 3) Back");
                     System.out.print("Choice: ");
                     // taking input3
                     input3 = sc.nextInt();
                     // handling invalid input
                     while(input3 <= 0 || input3 > 3) {
+                        displayDottedLine();
                         System.out.print("Invalid choice, enter again: ");
                         input3 = sc.nextInt();
                     }
                     // switching on input3
                     switch (input3) {
                         case 1:
-                            Authentication.createStudent();
+                            displayDottedLine();
+                            Authentication.createStudent(mainDataModel);
                             input = 3;
                             break;
                         case 2:
-                            Authentication.createInstructor();
+                            displayDottedLine();
+                            Authentication.createInstructor(mainDataModel);
                             input = 3;
                             break;
                         case 3:
+                            displayDottedLine();
                             break;
                     }
                     break;
 
                 case 3:
+                    displayDottedLine();
                     System.out.println("Goodbye!");
                     break;
             }
         } while(input != 3);
+    }
 
-         */
-        Student s = new Student("" , "");
-        s = data.getStudents().getFirst();
-        s.loadMainMenu();
-
-        /* Instructor i = new Instructor("" , "");
-         i = data.getInstructors().getFirst();
-         i.loadMainMenu();
-
-         */
+    private static void displayDottedLine() {
+        System.out.println("********************************************");
     }
 }
